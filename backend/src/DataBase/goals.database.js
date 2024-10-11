ea8caef624006cba33b17dd556dd5fb9952a86cb
Natalia Silva <nataliapsilva36@gmail.com>
2024-10-11 11:35:39 -0300

    feat: add goals database

---
+++ backend/src/DataBase/goals.database.js
@@ -0,0 +1,7 @@
+import { Goal } from "../Models/goal.models.js";
+
+export const goals = [
+    new Goal(1, "Melhorar a comunicação", "Ajudar o usuário a melhor sua fala e escrita", false),
+    new Goal(2, "Aumentar a socialização", "Fomentar interações positivas e treinar habilidades sociais", false),
+    new Goal(3, "Se desenvolver em um novo esporte", "Praticar um esporte para expressar sentimentos", true)
+];
