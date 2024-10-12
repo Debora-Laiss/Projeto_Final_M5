f5ba7b6973c068ea031b3395c4826a585264c6cc
Andre Nascimento <1914andre@gmail.com>
2024-10-12 20:37:36 -0300

    feat : adding logic for creating generic input for rendering assignment.

---
+++ frontend/src/components/login/input/input.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = () => {
+    ({
+        type,placeholder
+    }) => {
+        
+  return (
+     <div>
+       <input type = {type} placeholder = {placeholder} />
+     </div>
+  )
+   
+}}
+
+export default Input
