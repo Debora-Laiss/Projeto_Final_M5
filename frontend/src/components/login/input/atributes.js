bec0c013e5bc390fc173545431ddd239dd3116ce
Andre Nascimento <1914andre@gmail.com>
2024-10-12 20:29:42 -0300

    feat: adding atributes to inputs

---
+++ frontend/src/components/login/input/atributes.js
@@ -0,0 +1,13 @@
+const Atributes = [ 
+{
+    type: "text",
+    placeholder: "insira seu gmail"
+},
+
+{
+    type: "password",
+    placeholder:"insira sua senha"
+}
+]
+
+export default Atributes
