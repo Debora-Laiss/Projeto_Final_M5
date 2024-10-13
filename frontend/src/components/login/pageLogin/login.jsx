1c9cd543da82f25ae8648516b9f7653d1319c9aa
Andre Nascimento <1914andre@gmail.com>
2024-10-12 21:00:59 -0300

    feat: adding components of the components Title and Input

---
+++ frontend/src/components/login/pageLogin/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Input from '../input/input.jsx'
+import Title from '../title/title.jsx'
+
+
+const LoginPage = () => {
+  return (
+   <>
+      <section><Title/></section> 
+      <section><Input/></section>
+  
+      
+    </>
+  )
+}
+
+export default LoginPage 
