9a588dfc310cfd536d6866ff8d5c0deede76076b
Andre Nascimento <1914andre@gmail.com>
2024-10-12 21:17:03 -0300

    fix: adding tag form and class main

--- frontend/src/components/login/pageLogin/login.jsx
+++ frontend/src/components/login/pageLogin/login.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Input from '../input/input.jsx'
 import Title from '../title/title.jsx'
+import Button from '../button/button.jsx'
 
 
 const LoginPage = () => {
   return (
    <>
+   <form className ='main'>
       <section><Title/></section> 
       <section><Input/></section>
-  
+      <section><Button/></section>
+   </form>
       
     </>
   )
