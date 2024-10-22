52c489d03c3fe482342360fef9ab1d2a67e7007d
Andre Nascimento <1914andre@gmail.com>
2024-10-22 19:39:26 -0300

    feat: add cors

--- backend/src/config/corsConfig.js
+++ backend/src/config/corsConfig.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsConfig = {
-    origin: ['http://localhost:3000', 'http://localhost:5174'], // Aceitar múltiplas origens
+    origin: ['http://localhost:3000', 'http://localhost:5174', 'http://localhost:5173'], // Aceitar múltiplas origens
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 200 
