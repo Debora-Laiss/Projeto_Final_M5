d6c1b93f4315824fd8b2ff9a909a83d2b495479f
Felipe Souza <fn23886@gmail.com>
2024-10-22 20:40:34 -0300

    feat: login working

--- frontend/src/components/login/buttonGoogle/buttonProvider.jsx
+++ frontend/src/components/login/buttonGoogle/buttonProvider.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import React, { useEffect } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GoogleLogin } from '@react-oauth/google';
 import handleLogin from './button';
-
-
+import { useNavigate } from 'react-router-dom';
 
 const ButtonProvider = () => {
-    const clientId = '367692770833-6k9vracsbrmiptaj6alrjngaeaf0uqu8.apps.googleusercontent.com' 
-  
+  const clientId = '367692770833-6k9vracsbrmiptaj6alrjngaeaf0uqu8.apps.googleusercontent.com';
+  const navigate = useNavigate();
+
+
+  const onLoginSuccess = (response) => {
+    handleLogin(response);  
+    navigate('/home');     
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('3 segundos se passaram');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-       <GoogleOAuthProvider clientId={clientId}>
-            <GoogleLogin
-                onSuccess={handleLogin} 
-                onError={() => {
-                  console.log('Login falhou');
-                }}
-            />
-        </GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId={clientId}>
+        <GoogleLogin
+          onSuccess={onLoginSuccess}  
+          onError={() => {
+            console.log('Login falhou');
+          }}
+        />
+      </GoogleOAuthProvider>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonProvider
+export default ButtonProvider;
