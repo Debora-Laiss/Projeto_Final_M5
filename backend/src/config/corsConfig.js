aab8dd687bbc4932a8d92e1865ba48455a13e796
debora <deboralaisnm@gmail.com>
2024-10-24 17:18:44 -0300

    Feat: config cors

--- backend/src/config/corsConfig.js
+++ backend/src/config/corsConfig.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsConfig = {
-    origin: ['http://localhost:3000', 'http://localhost:5174','http://localhost:5173'], // Aceitar múltiplas origens
+    origin:  [ 'http://localhost:3000', 'http://localhost:5174','http://localhost:5173', 'https://projeto-final-m5-4.onrender.com' ], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 200 
